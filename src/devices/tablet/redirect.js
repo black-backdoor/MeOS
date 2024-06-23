c5187140b390bebf2b5d6725e598b3ad168b229f
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-06-23 22:24:47 +0200

    Create devices/tablet/

---
+++ src/devices/tablet/redirect.js
@@ -0,0 +1,25 @@
+/* 
+ * Redirects to / if screen size is too small
+ */
+
+window.onload = function() {
+    function popup() {
+        return confirm("Your screen size is too small for this content.");
+    }
+
+    if (window.innerWidth < 1100) {
+        // Redirect to mobile.html if screen width is less than 1100px
+        console.warn(`%c[Warning]%c Screen size is too small for content! min-width is 1100x; currently: ${window.innerWidth};`, 'color: red; font-weight: bold;', 'color: inherit;');
+        if(popup() === true) {
+            window.location.href = "/";
+        }
+    }
+
+    if (window.innerHeight < 650) {
+        // Redirect to mobile.html if screen height is less than 650px
+        console.warn(`%c[Warning]%c Screen size is too small for content! min-height is 650px; currently: ${window.innerHeight};`, 'color: red; font-weight: bold;', 'color: inherit;');
+        if(popup() === true) {
+            window.location.href = "/";
+        }
+    }
+};
