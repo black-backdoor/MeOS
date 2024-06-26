bd67f71f9bd31734e19729c8b6f5021c8bcb4cdd
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-06-26 10:01:20 +0200

    WINDOW DRAGING

--- src/components/windows.js
+++ src/components/windows.js
@@ -191,3 +191,92 @@ class AppWindow extends HTMLElement {
 }
 
 customElements.define('app-window', AppWindow);
+
+
+
+/* WINDOW DRAGING */
+document.addEventListener('DOMContentLoaded', function() {
+    const desktop = document.querySelector("#desktop");
+    const windows = document.querySelectorAll('app-window');
+    windows.forEach(function (windowElement) {
+        // DRAGGING WINDOWS (MOUSE) / (TOUCH)
+        const windowHead = windowElement.shadowRoot.querySelector('header');
+        const windowControls = windowElement.shadowRoot.querySelector('.controls');
+        windowHead.addEventListener('mousedown', startDragging);
+        windowHead.addEventListener('touchstart', startDragging);
+
+        function startDragging(e) {
+            // don't drag the window if the user clicks on the controls (close, minimize, maximize)
+            if (e.target == windowControls || windowControls.contains(e.target)) {
+                return;
+            }
+
+            // add dragging class to the window
+            windowElement.classList.add('dragging');
+            document.body.classList.add('window-dragging');
+
+            // Calculate the initial offset from the top-left corner of the window
+            let offsetX, offsetY;
+            if (e.type === 'mousedown') {
+                offsetX = e.clientX - windowElement.getBoundingClientRect().left;
+                offsetY = e.clientY - windowElement.getBoundingClientRect().top;
+            } else if (e.type === 'touchstart') {
+                offsetX = e.touches[0].clientX - windowElement.getBoundingClientRect().left;
+                offsetY = e.touches[0].clientY - windowElement.getBoundingClientRect().top;
+            }
+
+            if ( windowElement.classList.contains('fullscreen') ) { 
+                // ! order of these two lines is important !
+                // ? we need the width of the window in non-fullscreen mode since else the windowElement.offsetWidth will be the width of the screen
+                windowElement.classList.remove('fullscreen');
+                offsetX = (windowElement.offsetWidth / 2);
+            }
+
+            // Add a move event listener to update the window position
+            function moveWindow(e) {
+                let clientX, clientY;
+                if (e.type === 'mousemove') {
+                    clientX = e.clientX;
+                    clientY = e.clientY;
+                } else if (e.type === 'touchmove') {
+                    clientX = e.touches[0].clientX;
+                    clientY = e.touches[0].clientY;
+                }
+
+                // Set the window position based on the cursor position
+                let x = clientX - offsetX;
+                let y = clientY - offsetY;
+
+                // don't let the window go outside the screen
+                if(x < 0) x = 0;  // don't let the window go outside the left edge of the screen
+                if(y < 0) y = 0;  // don't let the window go outside the top edge of the screen
+
+                if(y > (desktop.offsetHeight - 50)) y = desktop.offsetHeight - 50;  // don't let the window go outside the bottom edge of the screen
+                if(x > (desktop.offsetWidth - 100)) x = desktop.offsetWidth - 100;  // don't let the window go outside the right edge of the screen
+
+                windowElement.style.left = x + 'px';
+                windowElement.style.top = y + 'px';
+            }
+
+            // Add a stop event listener to stop tracking cursor movement
+            function stopMoving() {
+                windowElement.classList.remove('dragging');
+                document.body.classList.remove('window-dragging');
+
+                // Remove the event listeners when the mouse button is released
+                document.removeEventListener('mousemove', moveWindow);
+                document.removeEventListener('mouseup', stopMoving);
+                document.removeEventListener('touchmove', moveWindow);
+                document.removeEventListener('touchend', stopMoving);
+
+                // TODO: save the position of the window
+            }
+
+            // Attach the event listeners
+            document.addEventListener('mousemove', moveWindow);
+            document.addEventListener('mouseup', stopMoving);
+            document.addEventListener('touchmove', moveWindow);
+            document.addEventListener('touchend', stopMoving);
+        }
+    });
+});
