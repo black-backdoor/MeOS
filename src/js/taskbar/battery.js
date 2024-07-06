902ae9df9b300bd28bde4e103e0fd51ce5a11c90
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-06 21:03:17 +0200

    Battery icon (#39)

--- src/js/taskbar/battery.js
+++ src/js/taskbar/battery.js
@@ -1,5 +1,5 @@
 /*
-    This script is used to display the battery status in the taskbar.
+    This script displays the battery status in the taskbar.
     Since the battery status is not supported in all browsers, we need to check if it is supported before using it.
     The battery status is updated every time the battery status changes.
 
@@ -7,117 +7,98 @@
     because the updateBatteryStatus function isn't (supposed to be) called by other scripts.
 */
 
-
-
 if ('getBattery' in navigator) {
     navigator.getBattery().then((battery) => {
         console.info('%c[setBatteryStatus]%c battery status is supported', 'color: lightgreen', 'color: inherit');
-        updateBatteryStatus();
-        battery.addEventListener('chargingchange', updateBatteryStatus);
-        battery.addEventListener('chargingtimechange', updateBatteryStatus);
-        battery.addEventListener('dischargingtimechange', updateBatteryStatus);
-        battery.addEventListener('levelchange', updateBatteryStatus);
+        updateBatteryStatus(battery);
+        battery.addEventListener('chargingchange', () => updateBatteryStatus(battery));
+        battery.addEventListener('chargingtimechange', () => updateBatteryStatus(battery));
+        battery.addEventListener('dischargingtimechange', () => updateBatteryStatus(battery));
+        battery.addEventListener('levelchange', () => updateBatteryStatus(battery));
     });
 } else {
-    const batteryStatus = document.querySelector('#taskbar > .menu > .battery');
     console.info('%c[setBatteryStatus]%c battery status is not supported', 'color: lightgreen', 'color: inherit');
+
+    const batteryStatus = document.querySelector('#taskbar > .menu > .battery');
     batteryStatus.title = 'Battery API is not supported';
+
+    const batteryIconStyle = document.querySelector('#taskbar > .menu > .battery > svg style');
+    batteryIconStyle.innerHTML = `
+        .fill-1, .fill-2, .fill-3, .fill-4, .fill-5, .fill-6, .fill-7, .fill-8, .fill-9, .fill-10 { display: none; }
+        .charging { display: none; }
+    `;
+
+    const batteryIconText = document.querySelector('#taskbar > .menu > .battery > svg .text');
+    batteryIconText.innerHTML = 'X';
 }
 
+function updateBatteryStatus(battery) {
+    const text_percent = false;
 
-
-function updateBatteryStatus() {
     function convertSecondsToTime(seconds) {
-        if (seconds == Infinity) return '∞';
+        if (seconds === Infinity) return '∞';
 
         const hours = Math.floor(seconds / 3600);
         const minutes = Math.floor((seconds % 3600) / 60);
-    
         return `${hours}h ${minutes}m`;
     }
 
 
-    const batteryStatus = document.querySelector('#taskbar > .menu > .battery');
-    const batteryIMG = document.querySelector('#taskbar > .menu > .battery img');
+    const { level, charging } = battery;
+    const percent = Math.round(level * 100);
+    let message = `Battery status: ${percent}% `;
 
-    if (batteryStatus == undefined || batteryIMG == undefined) {
-        console.warn('%c[setBatteryStatus]%c the battery icon was not found', 'color: lightgreen', 'color: inherit');
-
-        // if the battery icon is not found, try again in 1 second (could be that the dom hasn't loaded yet | but this should not happen -> script is deferred)
-        setTimeout(updateBatteryStatus, 1000);
-
-        return;
-    }
-
-    if ('getBattery' in navigator) {
-        navigator.getBattery().then((battery) => {
-            const { level, charging } = battery;
-            const percent = Math.round(level * 100);
-            
-            /* TWO EXAMPLES OF BATTERY STATUS MESSAGES
-            let message = `Battery status: ${percent}% `;
-
-            if (charging) {
-                if (battery.chargingTime === Infinity) {
-                    // Example: Battery status: 50% (plugged in)
-                    message += 'available (plugged in)';
-                } else {
-                    // Example: Battery status: 50% (2h 30m until full)
-                    message += `(${convertSecondsToTime(battery.chargingTime)} until full)`;
-                }
-            } else {
-                if (battery.dischargingTime === Infinity) {
-                    // Example: Battery status: 50% remaining
-                    message += 'remaining';
-                } else {
-                    // Example: Battery status: 50% (2h 30m left)
-                    message += `(${convertSecondsToTime(battery.dischargingTime)} left)`;
-                }
-            }
-            */
-            
-            let message;
-
-            if (charging) {
-                message = `Charging: ${percent}% `;
-                if (battery.chargingTime === Infinity) {
-                    // Example: 50% (plugged in)
-                    message += 'available (plugged in)';
-                } else {
-                    // Example: 50% (2h 30m until full)
-                    message += `(${convertSecondsToTime(battery.chargingTime)} until full)`;
-                }
-            } else {
-                message = `Discharging: ${percent}% `;
-                if (battery.dischargingTime === Infinity) {
-                    // Example: Discharging: 50% remaining
-                    message += 'remaining';
-                } else {
-                    // Example: Discharging: 50% (2h 30m left)
-                    message += `(${convertSecondsToTime(battery.dischargingTime)} left)`;
-                }
-            }
-
-            batteryStatus.title = message;
-
-            // DEBUG LOGS
-            console.debug(`%c[setBatteryStatus]%c battery title updated to: '${message}'`, 'color: lightgreen', 'color: inherit');
-            console.info(
-                `%c[setBatteryStatus]%c battery level: %c${percent}% %cand battery is %c${charging ? 'charging' : 'discharging'}`,
-                'color: lightgreen',
-                'color: inherit',
-                `color: ${percent <= 20 ? 'red' : percent <= 50 ? 'orange' : 'green'}`,
-                'color: inherit',
-                `color: ${charging ? 'green' : 'red'}`,
-            );
-
-            console.debug("IsCharging", charging);
-            console.debug("Percentage", percent);
-            console.debug("chargingTime", battery.chargingTime);
-            console.debug("dischargingTime", battery.dischargingTime);
-        });
+    if (charging) {
+        if (battery.chargingTime === Infinity) {
+            message += 'available (plugged in)';
+        } else {
+            message += `(${convertSecondsToTime(battery.chargingTime)} until full)`;
+        }
     } else {
-        console.debug('%c[setBatteryStatus]%c battery status updated: not supported', 'color: lightgreen', 'color: inherit');
-        batteryStatus.title = 'Battery status is not supported';
+        if (battery.dischargingTime === Infinity) {
+            message += 'remaining';
+        } else {
+            message += `(${convertSecondsToTime(battery.dischargingTime)} left)`;
+        }
     }
+
+    const batteryStatus = document.querySelector('#taskbar > .menu > .battery');
+    batteryStatus.title = message;
+        
+    const batteryIconText = document.querySelector('#taskbar > .menu > .battery > svg .text');
+    const batteryIconStyle = document.querySelector('#taskbar > .menu > .battery > svg style');
+
+    if(text_percent) {
+        batteryIconText.innerHTML = percent;
+        batteryIconStyle.innerHTML = `[class^="fill-"] { display: none; }`;
+    } else {
+        batteryIconText.innerHTML = '';
+        batteryIconStyle.innerHTML = '';
+
+        const fillLevel = Math.round(level * 10);
+        for (let i = 1; i <= 10; i++) {
+            if (i > fillLevel) {
+                batteryIconStyle.innerHTML += `.fill-${i} { display: none; }`;
+            }
+        }
+    }
+
+    if (!charging) {  batteryIconStyle.innerHTML += `.charging { display: none; }`; }
+
+
+    // Debug logs
+    console.debug(`%c[setBatteryStatus]%c battery title updated to: '${message}'`, 'color: lightgreen', 'color: inherit');
+    console.info(
+        `%c[setBatteryStatus]%c battery level: %c${percent}% %cand battery is %c${charging ? 'charging' : 'discharging'}`,
+        'color: lightgreen',
+        'color: inherit',
+        `color: ${percent <= 20 ? 'red' : percent <= 50 ? 'orange' : 'green'}`,
+        'color: inherit',
+        `color: ${charging ? 'green' : 'red'}`,
+    );
+
+    console.debug("IsCharging", charging);
+    console.debug("Percentage", percent);
+    console.debug("chargingTime", battery.chargingTime);
+    console.debug("dischargingTime", battery.dischargingTime);
 }
