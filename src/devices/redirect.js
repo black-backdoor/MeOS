a6528f112342975881b41b42dc470d1875ae5475
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-07 11:57:32 +0200

    -

--- src/devices/redirect.js
+++ src/devices/redirect.js
@@ -4,7 +4,7 @@ window.onload = function() {
         const minHeight = parseInt(document.body.getAttribute('data-min-height'), 10) || 0;
 
         if (window.innerWidth < minWidth || window.innerHeight < minHeight) {
-            console.warn(`%c[Warning]%c Screen size is too small for content! min-width is ${minWidth}px; min-height is ${minHeight}px; currently: ${window.innerWidth}px x ${window.innerHeight}px;`, 'color: red; font-weight: bold;', 'color: inherit;');
+            console.warn(`%c[Warning]%c Screen size is too small for content! min-width is ${minWidth}px; min-height is ${minHeight}px; currently: ${window.innerWidth}px x ${window.innerHeight}px;`, 'color: OrangeRed; font-weight: bold;', 'color: inherit;');
             return confirm("Your screen size is too small for this content. Do you want to go back?");
         }
         return false;
