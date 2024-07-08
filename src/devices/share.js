1f9833d92873305a23a4b655d58e335156101706
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-08 17:27:06 +0200

    Update devices/share.js

--- src/devices/share.js
+++ src/devices/share.js
@@ -2,11 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const shareButton = document.querySelector('#share');
     const shareButtonIMG = document.querySelector('#share img');
 
-    shareButton.onclick = async function() {
-        /* disable button to prevent multiple clicks */
-        shareButton.disabled = true;
+    const description = document.head.querySelector('meta[name="description"]').content ?? "Check out MeOS!";
 
-        /* console.debug('[Share] Share button clicked'); */
+    shareButton.onclick = async function() {
+        shareButton.disabled = true;
         
         const clipboard = function() {
             /* change button icon for user feedback */
@@ -16,17 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
             shareButtonIMG.src = newSRC;
 
             /* change button icon back */
-            setTimeout(() => { shareButtonIMG.src = oldSRC; }, 500);
+            setTimeout(() => { shareButtonIMG.src = oldSRC; shareButton.disabled = false; }, 500);
         };
 
         const error = function() {
             shareButton.style.backgroundColor = 'red';
         };
 
-        await share(clipboard, undefined, error, description = "Check out MeOS!");
-
-
-        /* re-enable button */
-        shareButton.disabled = false;
+        await shareURL(clipboard, undefined, error, description);        
     };
 });
