ae923737649a6e35fc7ba91b45c2b4bb2f6fab95
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-09 15:32:25 +0200

    Update performance.js
    first-contentful-paint

--- src/js/performance.js
+++ src/js/performance.js
@@ -1,5 +1,5 @@
 function logResourceTimings(resourceType) {
-    console.groupCollapsed(`%c[NETWORK]%c Network Resources: ${resourceType}`, 'color: gold', 'color: inherit;');
+    console.groupCollapsed(`%c[PERFORMANCE]%c Network Resources: ${resourceType}`, 'color: gold', 'color: inherit;');
 
     var resourceList = window.performance.getEntriesByType("resource");
     for (i = 0; i < resourceList.length; i++) {
@@ -12,19 +12,39 @@ function logResourceTimings(resourceType) {
 }
 
 function logPageTimings() {
-    let timing = window.performance.timing;
-    console.group("%c[PERFORMANCE]%c Page", "color: orange", "color: inherit");
-    console.debug("DNS Time: " + (timing.domainLookupEnd - timing.domainLookupStart));
-    console.debug("Connection Time: " + (timing.connectEnd - timing.connectStart));
-    console.debug("Request Time: " + (timing.responseEnd - timing.requestStart));
-    console.debug("Fetch Time: " + (timing.responseEnd - timing.fetchStart));
-    console.debug("Render Time: " + (timing.domComplete - timing.domLoading));
-    console.debug("User Time: " + (timing.loadEventEnd - timing.navigationStart));
+    function calculateColor(value, middle, max) {
+        if (value < middle) {
+            return 'green';
+        } else if (value >= middle && value < max) {
+            return 'orange';
+        } else {
+            return 'red';
+        }
+    }
+
+    console.group("%c[PERFORMANCE]%c Page", 'color: orange', 'color: inherit');
+    const page_load_time = performance.timing.loadEventEnd - performance.timing.fetchStart;
+    const page_navigation_time = performance.timing.loadEventEnd - performance.timing.navigationStart;
+    const dcl = performance.timing.domComplete - performance.timing.domInteractive;
+    console.info(`%c[PERFORMANCE]%c Page load time is %c${page_load_time}%c ms`, 'color: orange', 'color: inherit', `color: ${calculateColor(page_load_time, 1000, 3000)};`, 'color: inherit;');
+    console.info(`%c[PERFORMANCE]%c DOM Content Loaded (DCL): %c${dcl}%c ms`, 'color: orange', 'color: inherit', `color: ${calculateColor(dcl, 800, 1200)};`, 'color: inherit;');
+    console.info(`%c[PERFORMANCE]%c Page navigation time is ${page_navigation_time} ms`, 'color: orange', 'color: inherit');    
     console.groupEnd();
+
+    const paintObserver = new PerformanceObserver(list => {
+        const entries = list.getEntries();
+        entries.forEach(entry => {    
+            // The time to first-paint took 509 milliseconds.
+            // The time to first-contentful-paint took 509 milliseconds.
+            console.log(`%c[PERFORMANCE]%c The time to ${entry.name} took ${Math.round(entry.startTime)} milliseconds.`, 'color: orange', 'color: inherit');
+        });
+    });
+    
+    paintObserver.observe({ type: "paint", buffered: true });
 }
 
 function logPerformance() {
-    if (window.performance && window.performance.timing) {
+    if ('performance' in window) {
         logPageTimings();
     } else {
         console.info("Page Timing API is not present");
