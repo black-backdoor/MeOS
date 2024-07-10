4574b0590f48fdfff31bcac0e1f8784ff55d9d27
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-10 15:47:52 +0200

    Update lock/script.js

--- src/lock/script.js
+++ src/lock/script.js
@@ -1,23 +1,16 @@
+const users = {
+    'MeOS': 'meos',
+    'Admin': 'admin',
+    'root': 'toor',
+    'groot': 'groot',
+};
+
+const user = "MeOS";
+
+
+
+/* POPULATE USERS */
 document.addEventListener('DOMContentLoaded', () => {
-    /* SET USERNAME | QUERY PARAM */
-    const username = document.getElementById('username');
-        
-    const url = new URL(window.location.href);
-    const params = new URLSearchParams(url.search);
-    const usernameParam = params.get('username') || 'User';
-    username.innerText = usernameParam;
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    /* POPULATE USERS */
-    const users = {
-        'MeOS': 'meos',
-        'Admin': 'admin',
-        'root': 'toor',
-        'groot': 'groot',
-    };
-
     const usersElement = document.querySelector('.users');
     usersElement.innerHTML = '';
     Object.entries(users).forEach(([username, password]) => {
@@ -32,24 +25,25 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
+/* SWITCH USER */
 document.addEventListener('DOMContentLoaded', () => {
-    /* SWITCH USER */
     const usersElement = document.querySelector('.users');
     const userElements = usersElement.querySelectorAll('.user');
     userElements.forEach(userElement => {
         userElement.addEventListener('click', () => {
-            const selectedUser = userElement.querySelector('.username').innerText;
-            window.location.href = `/lock?username=${selectedUser}`;
+            const username = userElement.querySelector('.username').textContent;
+            console.log(`Switching to user: ${username}`);
+            // SWITCH USER
         });
     });
 });
 
 
 
+/* SHOW PASSWORD */
 document.addEventListener('DOMContentLoaded', () => {
     const passwordInput = document.getElementById('password');
-    
-    /* SHOW PASSWORD */
     const showPasswordButton = document.getElementById('show-password');
     const hidePassword = () => {
         passwordInput.setAttribute('type', 'password');
@@ -63,30 +57,4 @@ document.addEventListener('DOMContentLoaded', () => {
     showPasswordButton.addEventListener('mousedown', showPassword);
     showPasswordButton.addEventListener('mouseup', hidePassword);
     showPasswordButton.addEventListener('mouseleave', hidePassword);
-
-    /* CHECK PASSWORD */
-    const password = 'meos';
-    function checkPassword() {
-        const passwordValue = passwordInput.value;
-        passwordInput.value = '';
-        if (passwordValue === password) {
-            alert('Correct Password!');
-        } else {
-            alert('Incorrect Password!');
-        }
-    }
-
-    passwordInput.addEventListener('input', () => {
-        const passwordValue = passwordInput.value;
-        if (passwordValue.length == password.length) {
-            /* add delay to wait until you can be sure the user has finished typing */
-            checkPassword();
-        }
-    });
-
-    passwordInput.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            checkPassword();
-        }
-    });
 });
