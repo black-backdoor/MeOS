741fd057e310950edecd83f00b399f5c6244b040
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-14 16:46:07 +0200

    create progress component (#79)

---
+++ src/components/progress.js
@@ -0,0 +1,147 @@
+class UIProgress extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+
+        this.percent = 0;
+        this.label;
+
+        this.render();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    css() {
+        return `
+            /* COLORS */
+            :host {
+                --fill-color: #006fee;
+                --bg-color: #e9e9eb;
+                --text-color: rgb(17, 24, 28);
+            }
+            
+            :host {
+                height: 16px;
+                width: 200px;
+                display: flex;
+                flex-direction: column;
+            }
+
+            .label {
+                color: var(--text-color);
+                height: 16px;
+                font-size: 16px;
+                margin-bottom: 4px;
+            }
+            
+            .bar {
+                overflow: hidden;
+                width: 100%;
+                height: 100%;
+                max-height: 16px;
+                background-color: var(--bg-color);
+            }
+            
+            .fill {
+                position: relative;
+                transform: translateX(-${100 - this.percent}%);
+                transition-duration: .5s;
+                height: 100%;
+                background-color: var(--fill-color);
+            }
+
+
+            /* INDETERMINATE */
+            :host([isIndeterminate]) .fill {
+                transform: translateX(-30%);
+                transition-property: transform;
+                transition-timing-function: cubic-bezier(.4,0,.2,1);
+                animation:indeterminate-bar 1.5s cubic-bezier(.65,.815,.735,.395) infinite normal none running;
+            }
+            @keyframes indeterminate-bar {
+                0% {
+                    transform:translateX(-50%) scaleX(.2)
+                }
+                to {
+                    transform:translateX(100%) scaleX(1)
+                }
+            }
+
+
+            /* ROUND */
+            :host([bar-style="round"]) .bar {
+                border-radius: 9999px;
+            }
+            :host([bar-style="round"]) .fill {
+                border-radius: 9999px;
+            }
+            
+
+            /* NO ANIMATION */
+            :host([no-animation]) .fill {
+                transition-duration: 0s;
+            }
+
+            /* LABEL */
+            .label {
+                display: none;
+            }
+            :host([label]) .label {
+                display: inherit;
+            }
+        `;
+    }
+
+    template() {
+        return `
+            <span class="label"></span>
+            <div class="bar">
+                <div class="fill"></div>
+            </div>
+        `;
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>${this.css()}</style>
+            ${this.template()}
+        `;
+
+        this.update();
+    }
+
+    update() {
+        this.shadowRoot.querySelector('.fill').style.transform = `translateX(-${100 - this.percent}%)`;
+        this.shadowRoot.querySelector('.label').textContent = this.label;
+    }
+
+
+    static get observedAttributes() {
+        return ['percent', 'label'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'percent') {
+            newValue = Number(newValue);
+            if (isNaN(newValue)) { return; }
+            if (newValue === null) { return; }
+            if (newValue === oldValue) { return; }
+            if (newValue < 0) { newValue = 0; }
+            if (newValue > 100) { newValue = 100; }
+
+            this.percent = newValue;
+            this.update();
+        }
+
+        if (name === 'label') {
+            console.log('label', newValue);
+            this.label = newValue;
+            this.update();
+        }
+    }
+}
+
+customElements.define('ui-progress', UIProgress);
