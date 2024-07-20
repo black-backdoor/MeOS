a1cf44196ea272babbd246f49fe1e8e7f2d8908f
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-20 12:29:19 +0200

    popup

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -11,12 +11,12 @@ Made by @black-backdoor
 `
 );
 
-
+/*
 document.addEventListener("DOMContentLoaded", () => {
 
     // WELCOME NOTIFICATION (PUSHED TO DESKTOP)
     if (sessionStorage && sessionStorage.getItem("welcome-notification") === null) {
-
+        
         sendNotification(
             "Welcome to MeOS!",
             "This is a simple operating system built with HTML, CSS, and JavaScript.",
@@ -27,5 +27,30 @@ document.addEventListener("DOMContentLoaded", () => {
         ); 
 
         sessionStorage.setItem("welcome-notification", true);
+    }
+});
+*/
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage && localStorage.getItem("fullscreenPopup") === null) {
+        const html = `
+        <desktop-popup_yes_no
+            id="fullscreenPopup"
+            position
+            icon="/assets/expand-arrows.svg"
+            message_title="Fullscreen"
+            message="MeOS best works in fullscreen mode. Do you want to enable fullscreen mode?"
+        ></desktop-popup_yes_no>
+        `;
+
+        document.body.insertAdjacentHTML("beforeend", html);
+        document.querySelector("#fullscreenPopup").addEventListener("close", (event) => {
+            if (event.detail === "ok") {
+                document.documentElement.requestFullscreen();
+            }
+
+            localStorage.setItem("fullscreenPopup", true);
+        });
     }
 });
